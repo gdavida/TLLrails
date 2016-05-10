88afa93e38134632e4baf8083539f6cc13d03624
Davida Gaffney <gdavida@gmail.com>
2016-05-10 02:42:10 -0500

    sorcery form working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to :users_path, notice: "Saved"
+      redirect_to users_path, notice: "Saved"
     else
       render :new
     end
