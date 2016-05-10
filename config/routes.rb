084299040b4a409af56a5dce88c1d4edce54c5cd
Davida Gaffney <gdavida@gmail.com>
2016-05-10 03:35:01 -0500

    new session form working

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  root :to => 'users#index'
 
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
 
-get 'login' => 'sessions#new', :as => :login
-post 'logout' => 'sessions#destroy', :as => :logout
+  get 'login' => 'sessions#new', :as => :login
+  post 'logout' => 'sessions#destroy', :as => :logout
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
