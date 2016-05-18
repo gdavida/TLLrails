5b3cfa7e50612182d5c60864f9503c07d6724903
Davida Gaffney <gdavida@gmail.com>
2016-05-18 08:07:22 -0500

    add viewport meta tag

--- db/migrate/20160510185503_create_toys.rb
+++ db/migrate/20160510185503_create_toys.rb
@@ -1,12 +1,16 @@
 class CreateToys < ActiveRecord::Migration
   def change
     create_table :toys do |t|
-      t.string :name, null: false
-      t.string :upc
-      t.string :toy_image
+      t.string  :name, null: false
+      t.string  :upc
+      t.string  :toy_image
       t.integer :age_group_id
       t.integer :box_id
       t.integer :pieces, null: false
+      t.string  :manufacturer
+      t.string  :price
+      t.string  :purchased_from
+      t.string  :country_of_origin
 
       t.timestamps null: false
 
--- db/schema.rb
+++ db/schema.rb
@@ -41,14 +41,18 @@ ActiveRecord::Schema.define(version: 20160512152245) do
   end
 
   create_table "toys", force: :cascade do |t|
-    t.string   "name",         null: false
+    t.string   "name",              null: false
     t.string   "upc"
     t.string   "toy_image"
     t.integer  "age_group_id"
     t.integer  "box_id"
-    t.integer  "pieces",       null: false
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.integer  "pieces",            null: false
+    t.string   "manufacturer"
+    t.string   "price"
+    t.string   "purchased_from"
+    t.string   "country_of_origin"
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
   end
 
   create_table "users", force: :cascade do |t|
